62b32ccea80a113133fbdf2e65c15c1e0335bec0
Ben <benawhiting@gmail.com>
2022-06-17 15:31:12 -0700

    Start forming calculator logic

--- script.js
+++ script.js
@@ -1,5 +1,122 @@
-let operator = null;
-let answer = null;
+let operator;
+let answer;
+let input1;
+let input2;
+let division = false;
+
+let screen = document.querySelector('#text');
+const buttons = document.querySelectorAll('button');
+
+// Adds the button's text content to the screen and receives first argument
+function firstArg() {   
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            switch (button.id) {
+                case 'one':
+                    screen.textContent += 1;
+                    break;
+                case 'two':
+                    screen.textContent += 2;
+                    break;
+                case 'three':
+                    screen.textContent += 3;
+                    break;
+                case 'four':
+                    screen.textContent += 4;
+                    break;
+                case 'five':
+                    screen.textContent += 5;
+                    break;
+                case 'six':
+                    screen.textContent += 6;
+                    break;
+                case 'seven':
+                    screen.textContent += 7;
+                    break;
+                case 'eight':
+                    screen.textContent += 8;
+                    break;
+                case 'nine':
+                    screen.textContent += 9;
+                    break;
+                case 'zero':
+                    screen.textContent += 0;
+                    break;
+                case 'clear':
+                    screen.textContent = 0;
+                    break;
+                case 'slash':
+                    division = true;
+                    input1 = screen.textContent;
+                    secondArg();
+                    break;
+                default:
+                    console.log("Still in function 1.");
+
+            }
+
+            console.log(input1, input2);
+            console.log("Still in function 1");
+        });
+    });
+}
+
+// Adds the button's content to the screen and receives the second argument
+function secondArg() {
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            screen.textContent = "";
+    
+            switch (button.id) {
+                case 'one':
+                    screen.textContent += 1;
+                    break;
+                case 'two':
+                    screen.textContent += 2;
+                    break;
+                case 'three':
+                    screen.textContent += 3;
+                    break;
+                case 'four':
+                    screen.textContent += 4;
+                    break;
+                case 'five':
+                    screen.textContent += 5;
+                    break;
+                case 'six':
+                    screen.textContent += 6;
+                    break;
+                case 'seven':
+                    screen.textContent += 7;
+                    break;
+                case 'eight':
+                    screen.textContent += 8;
+                    break;
+                case 'nine':
+                    screen.textContent += 9;
+                    break;
+                case 'zero':
+                    screen.textContent += 0;
+                    break;
+                case 'clear':
+                    screen.textContent = 0;
+                    break;
+                case 'equals':
+                    if (division == true) {
+                        operator = "slash";
+                        operate(input1, input2);
+                    } else {
+                        console.log("Woops");
+                    }
+                    break;
+                default:
+                    console.log("Something went wrong.");
+            }
+
+            console.log(input1, input2);
+        });
+    });
+}
 
 function add(x, y) {
     answer = x + y;
@@ -17,8 +134,9 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
+    console.log(x, y);
     answer = x / y;
-    return answer;
+    screen.textContent = answer;
 }
 
 // Return answer to user
@@ -33,9 +151,15 @@ function operate(x, y) {
         if (y == 0) {
             return "Fool";
         } else {
-            return divide(x, y);
+            divide(x, y);
         }
     } else {
         return "Error";
     }
+}
+
+if (input1 == null) {
+    firstArg();
+} else {
+    secondArg();
 }
