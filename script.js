b66f17c01dba081a55bed04abb4e35fe70061139
Ben <benawhiting@gmail.com>
2022-06-17 17:17:29 -0700

    Remove secondArg(); was redundant

--- script.js
+++ script.js
@@ -61,63 +61,6 @@ function firstArg() {
     });
 }
 
-// Adds the button's content to the screen and receives the second argument
-function secondArg() {
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            screen.textContent = "";
-    
-            switch (button.id) {
-                case 'one':
-                    screen.textContent += 1;
-                    break;
-                case 'two':
-                    screen.textContent += 2;
-                    break;
-                case 'three':
-                    screen.textContent += 3;
-                    break;
-                case 'four':
-                    screen.textContent += 4;
-                    break;
-                case 'five':
-                    screen.textContent += 5;
-                    break;
-                case 'six':
-                    screen.textContent += 6;
-                    break;
-                case 'seven':
-                    screen.textContent += 7;
-                    break;
-                case 'eight':
-                    screen.textContent += 8;
-                    break;
-                case 'nine':
-                    screen.textContent += 9;
-                    break;
-                case 'zero':
-                    screen.textContent += 0;
-                    break;
-                case 'clear':
-                    screen.textContent = 0;
-                    break;
-                case 'equals':
-                    if (division == true) {
-                        operator = "slash";
-                        operate(input1, input2);
-                    } else {
-                        console.log("Woops");
-                    }
-                    break;
-                default:
-                    console.log("Something went wrong.");
-            }
-
-            console.log(input1, input2);
-        });
-    });
-}
-
 function add(x, y) {
     answer = x + y;
     return answer;
@@ -158,8 +101,4 @@ function operate(x, y) {
     }
 }
 
-if (input1 == null) {
-    firstArg();
-} else {
-    secondArg();
-}
+firstArg();
